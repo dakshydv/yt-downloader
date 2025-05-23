4e198ae3eceea68a1802af4d6bfc33967d4b8065
DakshYdv <daksh.ydv2411@gmail.com>
2025-05-23 08:32:17 +0530

    allowed-origin updated

--- src/app/api/v1/route.ts
+++ src/app/api/v1/route.ts
@@ -1,48 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 import ytdl from "@distube/ytdl-core";
-import os from 'os'
+import os from "os";
 import path from "path";
 
-const url: string = process.env.NEXT_PUBLIC_DOWNLOAD_URL || '';
+const url: string = process.env.NEXT_PUBLIC_DOWNLOAD_URL || "";
 
-const ALLOWED_ORIGIN =
-  process.env.NODE_ENV === 'production'
-    ? url
-    : '*';
+const ALLOWED_ORIGIN = url || "*";
 
 export async function OPTIONS() {
   return new Response(null, {
     status: 200,
     headers: {
-      'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Credentials': 'true',
+      "Access-Control-Allow-Origin": ALLOWED_ORIGIN,
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
     },
   });
 }
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  let title: string = ''
-  const downloadPath = path.join(os.homedir(), 'Downloads');
+  let title: string = "";
+  const downloadPath = path.join(os.homedir(), "Downloads");
 
   try {
-    await ytdl.getBasicInfo(body.url).then(info => {
-      title = info.videoDetails.title
+    await ytdl.getBasicInfo(body.url).then((info) => {
+      title = info.videoDetails.title;
     });
-  
-    ytdl(body.url).pipe(fs.createWriteStream(`${downloadPath}/${title !== '' ? title : 'video'}.mp4`));
-  
-    return NextResponse.json({
-      status: 200,
-      message: "video downloaded",
-    },
-    {
-      headers: { 'Access-Control-Allow-Origin': ALLOWED_ORIGIN }
-    }
-  );
+
+    ytdl(body.url).pipe(
+      fs.createWriteStream(
+        `${downloadPath}/${title !== "" ? title : "video"}.mp4`
+      )
+    );
+
+    return NextResponse.json(
+      {
+        status: 200,
+        message: "video downloaded",
+      },
+      {
+        headers: { "Access-Control-Allow-Origin": ALLOWED_ORIGIN },
+      }
+    );
   } catch (error) {
     console.log(error);
   }
